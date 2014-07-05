774a17051e10a5be52586a7813314e600e64c568
Andrey Byelogurov <byelogurov@gmail.com>
2014-07-05 12:18:43 -0700

    First commit

---
+++ Free Sharp Player/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Collections.Specialized;
+using System.Threading;
+using NAudio;
+using NAudio.Wave;
+using System.Diagnostics;
+using System.Timers;
+
+namespace Free_Sharp_Player {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	/// 
+
+
+	public partial class MainWindow : Window {
+		[DllImport("kernel32")]
+		static extern bool AllocConsole();
+
+		Mp3Streamer theStreamer;
+
+		public MainWindow() {
+			InitializeComponent();
+
+			AllocConsole();
+
+
+
+			String address = "";
+
+			using (WebClient wb = new WebClient()) {
+				NameValueCollection data = new NameValueCollection();
+				data["sid"] = "1"; //quality, 1 == 128, 2 == 360, 3 == 80
+
+				//TODO: add ui to alert people this is loading
+				wb.DownloadProgressChanged += (o, e) => {
+					Console.WriteLine("S: " + e.BytesReceived);
+				};
+
+				Byte[] response = wb.UploadValues("http://radio.everfreeradio.com:5800/listen.pls", "POST", data);
+				
+				string[] responseData = System.Text.Encoding.UTF8.GetString(response, 0, response.Length).Split("\n".ToCharArray(), StringSplitOptions.None);
+
+
+				//Todo: timeout, check for valid return data, find the adress in more dynamic way.
+				address = responseData[2].Split("=".ToCharArray())[1];
+
+				Console.WriteLine("Address: " + address + ":" + port);
+			}
+
+			theStreamer = new Mp3Streamer(address);
+
+		}
+
+		/*//TODO: make this async?>
+		private async void listen() {
+			//TODO: handle timeouts.
+			int bytes = 1;
+			while (bytes != 0) { //kills the listener when we D/C
+				Byte[] data = new Byte[1024];
+
+				try {
+					//if (connection.GetStream().DataAvailable)
+					bytes = await connection.GetStream().ReadAsync(data, 0, data.Length);
+				} catch (ObjectDisposedException) {
+					break;
+				}
+
+				if (data != null) {
+					string[] responseData = System.Text.Encoding.UTF8.GetString(data, 0, bytes).Split(new string[] { "\r\n" }, StringSplitOptions.None);
+
+
+					foreach (String s in responseData) {
+						Console.WriteLine(s);
+					}
+
+					//i++;
+				}
+			}
+		}*/
+
+		
+		private void Play_Click(object sender, RoutedEventArgs e) {
+			// bufferedWaveProvider
+			//Play();
+		}
+
+		private void Pause_Click(object sender, RoutedEventArgs e) {
+			//Pause();
+		}
+
+		private void Stop_Click(object sender, RoutedEventArgs e) {
+			//StopPlayback();
+		}
+	}
+}
---
+++ Free Sharp Player/Streamer.cs
@@ -0,0 +1,192 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Free_Sharp_Player {
+	using Timer = System.Timers.Timer;
+	class Mp3Streamer {
+		enum StreamingPlaybackState { Stopped, Playing, Buffering, Paused }
+
+
+		private volatile StreamingPlaybackState playbackState { get; set; }
+		private bool endOfStream { get; private set; }
+
+		private HttpWebRequest webRequest;
+		private IWavePlayer waveOut;
+		private Timer waveLoader;
+		private VolumeWaveProvider16 volumeProvider;
+		private BufferedWaveProvider bufferedWaveProvider;
+		private String Address;
+
+		public Mp3Streamer(String address) {
+			waveLoader = new Timer(250);
+			waveLoader.Elapsed += waveLoader_Tick;
+			waveLoader.AutoReset = true;
+
+			Address = address;
+
+		}
+
+		public void Play() {
+			ThreadPool.QueueUserWorkItem(StreamMp3, Address);
+
+			waveOut.Play();
+			Debug.WriteLine(String.Format("Started playing, waveOut.PlaybackState={0}", waveOut.PlaybackState));
+			playbackState = StreamingPlaybackState.Playing;
+
+		}
+
+				private void StopPlayback() {
+			if (playbackState != StreamingPlaybackState.Stopped) {
+				if (!fullyDownloaded) {
+					webRequest.Abort();
+				}
+
+				playbackState = StreamingPlaybackState.Stopped;
+				if (waveOut != null) {
+					waveOut.Stop();
+					waveOut.Dispose();
+					waveOut = null;
+				}
+				timer1.Enabled = false;
+				// n.b. streaming thread may not yet have exited
+				Thread.Sleep(500);
+				ShowBufferState(0);
+			}
+		}
+		private void Pause() {
+			playbackState = StreamingPlaybackState.Buffering;
+			waveOut.Pause();
+			Debug.WriteLine(String.Format("Paused to buffer, waveOut.PlaybackState={0}", waveOut.PlaybackState));
+		}
+
+
+		private void StreamMp3(object state) {
+			fullyDownloaded = false;
+			String url = (String)state;
+			webRequest = (HttpWebRequest)WebRequest.Create(url);
+			HttpWebResponse resp;
+			try {
+				//webRequest.KeepAlive = false;
+				resp = (HttpWebResponse)webRequest.GetResponse();
+			} catch (WebException e) {
+				if (e.Status != WebExceptionStatus.RequestCanceled) {
+					ShowError(e.Message);
+				}
+				return;
+			}
+			Byte[] buffer = new Byte[16384 * 4]; // needs to be big enough to hold a decompressed frame
+
+			IMp3FrameDecompressor decompressor = null;
+			try {
+				using (Stream responseStream = resp.GetResponseStream()) {
+					ReadFullyStream readFullyStream = new ReadFullyStream(responseStream);
+					do {
+						if (IsBufferNearlyFull) {
+							Debug.WriteLine("Buffer getting full, taking a break");
+							Thread.Sleep(500);
+						} else {
+							Mp3Frame frame;
+							try {
+								frame = Mp3Frame.LoadFromStream(readFullyStream);
+							} catch (EndOfStreamException) {
+								fullyDownloaded = true;
+								// reached the end of the MP3 file / stream
+								break;
+							} catch (WebException) {
+								// probably we have aborted download from the GUI thread
+								break;
+							}
+							if (decompressor == null) {
+								// don't think these details matter too much - just help ACM select the right codec
+								// however, the buffered provider doesn't know what sample rate it is working at
+								// until we have a frame
+								decompressor = CreateFrameDecompressor(frame);
+								bufferedWaveProvider = new BufferedWaveProvider(decompressor.OutputFormat);
+								bufferedWaveProvider.BufferDuration = TimeSpan.FromSeconds(20); // allow us to get well ahead of ourselves
+								//this.bufferedWaveProvider.BufferedDuration = 250;
+							}
+							int decompressed = decompressor.DecompressFrame(frame, buffer, 0);
+							//Debug.WriteLine(String.Format("Decompressed a frame {0}", decompressed));
+							bufferedWaveProvider.AddSamples(buffer, 0, decompressed);
+						}
+
+					} while (playbackState != StreamingPlaybackState.Stopped);
+					Debug.WriteLine("Exiting");
+					// was doing this in a finally block, but for some reason
+					// we are hanging on response stream .Dispose so never get there
+					decompressor.Dispose();
+				}
+			} finally {
+				if (decompressor != null) {
+					decompressor.Dispose();
+				}
+			}
+		}
+
+		private static IMp3FrameDecompressor CreateFrameDecompressor(Mp3Frame frame) {
+			WaveFormat waveFormat = new Mp3WaveFormat(frame.SampleRate, frame.ChannelMode == ChannelMode.Mono ? 1 : 2,
+				frame.FrameLength, frame.BitRate);
+			return new AcmMp3FrameDecompressor(waveFormat);
+		}
+
+
+		private bool IsBufferNearlyFull {
+			get {
+				return bufferedWaveProvider != null &&
+					   bufferedWaveProvider.BufferLength - bufferedWaveProvider.BufferedBytes
+					   < bufferedWaveProvider.WaveFormat.AverageBytesPerSecond / 4;
+			}
+		}
+
+		private void waveLoader_Tick(object sender, EventArgs e) {
+			if (playbackState != StreamingPlaybackState.Stopped) {
+				if (waveOut == null && bufferedWaveProvider != null) {
+					Debug.WriteLine("Creating WaveOut Device");
+					waveOut = new WaveOut();
+					//waveOut.PlaybackStopped += OnPlaybackStopped;
+					volumeProvider = new VolumeWaveProvider16(bufferedWaveProvider);
+					//volumeProvider.Volume = volumeSlider1.Volume;
+					waveOut.Init(volumeProvider);
+					//TODO: Buffer Length
+					/*this.Dispatcher.Invoke(() => {
+						BufferLength.Maximum = (int)bufferedWaveProvider.BufferDuration.TotalMilliseconds;
+					});*/
+				} else if (bufferedWaveProvider != null) {
+					var bufferedSeconds = bufferedWaveProvider.BufferedDuration.TotalSeconds;
+					ShowBufferState(bufferedSeconds);
+					// make it stutter less if we buffer up a decent amount before playing
+					if (bufferedSeconds < 0.5 && playbackState == StreamingPlaybackState.Playing && !fullyDownloaded) {
+						Pause();
+					} else if (bufferedSeconds > 4 && playbackState == StreamingPlaybackState.Buffering) {
+						Play();
+					} else if (fullyDownloaded && bufferedSeconds == 0) {
+						Debug.WriteLine("Reached end of stream");
+						StopPlayback();
+					}
+				}
+
+			}
+		}
+		private void ShowBufferState(double totalSeconds) {
+			//labelBuffered.Text = String.Format("{0:0.0}s", totalSeconds);
+			/*this.Dispatcher.Invoke(() => {
+				BufferLength.Value = (int)(totalSeconds * 1000);
+			});*/
+		}
+
+
+		private void ShowError(String msg) {
+			MessageBox.Show(msg, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+		}
+
+	}
+}
